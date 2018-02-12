535585b088aaea4ae0536386ef346e3a82046620
Fernando Pinto <f6rnando@gmail.com>
2018-02-12 10:00:35 -0800

    Having the webmaster credentials on an external properties file

--- src/main/java/com/f6rnando/DevopsApplication.java
+++ src/main/java/com/f6rnando/DevopsApplication.java
@@ -10,6 +10,7 @@ import com.f6rnando.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,17 +27,25 @@ public class DevopsApplication implements CommandLineRunner {
 	@Autowired
 	private UserService userService;
 
+	@Value("${webmaster.username}")
+	private String webmasterUsername;
+
+    @Value("${webmaster.password}")
+	private String webmasterPassword;
+
+    @Value("${webmaster.email}")
+	private String webmasterEmail;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		String username = "proUser1";
-		String email = "prouser1@f6rnando.com";
-		User user = UserUtils.createBasicUser(username, email);
+		User user = UserUtils.createBasicUser(webmasterUsername, webmasterEmail);
+		user.setPassword(webmasterPassword);
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user, new Role(RolesEnum.PRO)));
+		userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
 		logger.debug("Creating a new user with username {}", user.getUsername());
 		userService.createUser(user, PlansEnum.PRO, userRoles);
 		logger.info("User {} created", user.getUsername());
